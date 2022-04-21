0ea3eda79f09ed846efdbe1d21d03d450a9a06a4
Morgan Lee <mhlee4@dons.usfca.edu>
2022-04-21 12:56:10 -0700

    added npc interactions

--- sketch.js
+++ sketch.js
@@ -123,7 +123,7 @@ function setup() {
   textFont('roc-grotesk');
 
   // NEW: line for debugging
-  adventureManager.changeState("Tech1");
+  // adventureManager.changeState("Street3");
 }
 
 // Adventure manager handles it all!
@@ -151,6 +151,10 @@ function draw() {
   } 
 
   avatarReset()
+}
+
+function keyPressed() {
+  adventureManager.keyPressed();
 }
 
 //--- TEMPLATE STUFF: Don't change 
@@ -347,21 +351,72 @@ class InstructionsScreen extends PNGRoom {
 
 class ChinaRoom extends PNGRoom {
   preload() {
-    // load sprites from advanced avatar class
-    // this.door = new StaticSprite("Door", 900, 700, 'assets/door.png');
-    //MAKE SURE TO PUT CODE IN CHECK OVERLAPS
+    // define class varibles here, load images or anything else
+    this.npc1 = new NPC("Auntie", width/2 - 10, height/2 + 50, 'assets/npc-auntie.png');
+    this.npc1.addSingleInteraction("Hello sweetie!");
+    this.npc1.addSingleInteraction("It\'s been so long since I\'ve seen you!");
+    this.npc1.addSingleInteraction("You look like you\'ve been eating a lot...");
+    this.npc1.addSingleInteraction("Getting a little chubby are we?");
+    this.npc1.addSingleInteraction("Your skin is so much darker too.");
+    this.npc1.addSingleInteraction("Do you have a boyfriend yet?");
+    this.npc1.addSingleInteraction("Are you getting good grades?");
+    this.npc1.addSingleInteraction("Have you seen Vivian? She\'s fat now.");
+    this.npc1.addSingleInteraction("But you should be more like her,");
+    this.npc1.addSingleInteraction("she has a boyfriend and a good job.");
+    this.npc1.addSingleInteraction("Why does you Chinese sound so bad?");
+    this.npc1.addSingleInteraction("You need to remember your roots...");
+    this.npc1.addSingleInteraction("Oh you have to go? Okay sweetie,");
+    this.npc1.addSingleInteraction("Say hi to your mom and dad for me,");
+    this.npc1.addSingleInteraction("you should come over for dinner!");
+    this.npc1.addSingleInteraction("You need to eat more.");
     
+    // setup flag, seto false
+    this.hasSetup = false;
   }
 
   // call the PNGRoom superclass's draw function to draw the background image
   // and draw our code adter this
   draw() {
+    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    if( this.hasSetup === false ) {
+      // setup NPC 1
+      this.npc1.setup();
+      this.npc1.setPromptLocation(0,-170);
+
+      this.hasSetup = true; 
+    }
+
     // this calls PNGRoom.draw()
-    
     super.draw();
 
-    // Add your code here
-    image(auntie, width/2, height/3, 100, 324);
+    // draw our NPCs
+    drawSprite(this.npc1.sprite);
+
+    // When you have multiple NPCs, you can add them to an array and have a function 
+    // iterate through it to call this function for each more concisely.
+    this.npc1.displayInteractPrompt(playerAvatar);
+  }
+
+  // custom code here to do stuff upon exiting room
+  unload() {
+    // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
+  }
+
+  // custom code here to do stuff upon entering room
+  load() {
+    // pass to PNGRoom to load image
+    super.load();
+    
+    // Add custom code here for unloading
+  }
+
+  keyPressed() {
+    if(key === ' ') {
+      if(this.npc1.isInteracting(playerAvatar)) {
+        this.npc1.continueInteraction();
+      }
+    }
   }
 }
 
@@ -375,14 +430,10 @@ class Tech1Room extends PNGRoom {
     this.npc1.addSingleInteraction("Feel free to walk around and talk to everyone!");
 
     this.npc2 = new NPC("Tech1", width - 400, height/2, 'assets/npc-tech1.png');
-    // this.npc2.addStandingAnimation('assets/sun1.png', 'assets/sun3.png');
     this.npc2.addSingleInteraction("Hello! I\'m Alan, I just started working here.");
-    this.npc2.addSingleInteraction("I graduated suma cum laude last year.");
-    this.npc2.addSingleInteraction("I was also president of my campus' Asian Scholar Association.");
-    this.npc2.addSingleInteraction("I\'m excited to see how I progress in a few years!");
-    
-    //this.npc2.setupQuest("Star", "Thanks! This is just what I needed", "I didn't ask for that!");
-
+    this.npc2.addSingleInteraction("I graduated summa cum laude last year.");
+    this.npc2.addSingleInteraction("I was also president of the Asian Scholar Association.");
+    this.npc2.addSingleInteraction("I\'m excited to see how I progress in the future!");
     
     // setup flag, seto false
     this.hasSetup = false;
@@ -395,11 +446,11 @@ class Tech1Room extends PNGRoom {
     if( this.hasSetup === false ) {
       // setup NPC 1
       this.npc1.setup();
-      this.npc1.setPromptLocation(0,-100);
+      this.npc1.setPromptLocation(0,-170);
       
       // setup NPC 2
       this.npc2.setup();
-      this.npc2.setPromptLocation(0,-100);
+      this.npc2.setPromptLocation(0,-170);
 
       this.hasSetup = true; 
     }
@@ -420,6 +471,7 @@ class Tech1Room extends PNGRoom {
   // custom code here to do stuff upon exiting room
   unload() {
     // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
     this.npc2.resetInteraction();
   }
 
@@ -446,71 +498,348 @@ class Tech1Room extends PNGRoom {
 class Tech2Room extends PNGRoom {
   preload() {
     // define class varibles here, load images or anything else
+    this.npc1 = new NPC("Tech2", 255, 330, 'assets/npc-tech2.png');
+    this.npc1.addSingleInteraction("Hi, I'm Paul.");
+    this.npc1.addSingleInteraction("Sorry I can\'t talk for long.");
+    this.npc1.addSingleInteraction("I had to pick up more hours this month.");
+    this.npc1.addSingleInteraction("My wife's nail salon job doesn\'t pay much.");
+
+    this.npc2 = new NPC("Tech3", width - 500, height/2, 'assets/npc-tech3.png');
+    this.npc2.addSingleInteraction("Are you visiting?");
+    this.npc2.addSingleInteraction("Did you meet Alan at the front?");
+    this.npc2.addSingleInteraction("I feel bad for that kid.");
+    this.npc2.addSingleInteraction("That was me like 20 years ago,");
+    this.npc2.addSingleInteraction("I\'ve never gotten anything more than a raise.");
+    this.npc2.addSingleInteraction("People like us won\'t be CEO anytime soon.");
+
+    this.npc3 = new NPC("Tech4", width- 255, 330, 'assets/npc-tech4.png');
+    this.npc3.addSingleInteraction("Hi, I\'m Don,");
+    this.npc3.addSingleInteraction("Sorry about Greg, he is a little jaded.");
+    this.npc3.addSingleInteraction("I believe there will be change!");
+    this.npc3.addSingleInteraction("We just need to be grateful for");
+    this.npc3.addSingleInteraction("the opportunities we have now.");
+    
+    // setup flag, seto false
+    this.hasSetup = false;
   }
 
   // call the PNGRoom superclass's draw function to draw the background image
   // and draw our code adter this
   draw() {
+    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    if( this.hasSetup === false ) {
+      // setup NPC 1
+      this.npc1.setup();
+      this.npc1.setPromptLocation(0,-170);
+      
+      // setup NPC 2
+      this.npc2.setup();
+      this.npc2.setPromptLocation(0,-170);
+
+      // setup NPC 3
+      this.npc3.setup();
+      this.npc3.setPromptLocation(0,-170)
+
+      this.hasSetup = true; 
+    }
+
     // this calls PNGRoom.draw()
     super.draw();
 
-    // Add your code here
-    image(tech2, width/3, height/2);
-    image(tech3, width - 500, height/2);
-    image(tech4, width/2, height/2);
+    // draw our NPCs
+    drawSprite(this.npc1.sprite);
+    drawSprite(this.npc2.sprite);
+    drawSprite(this.npc3.sprite);
+
+    // When you have multiple NPCs, you can add them to an array and have a function 
+    // iterate through it to call this function for each more concisely.
+    this.npc1.displayInteractPrompt(playerAvatar);
+    this.npc2.displayInteractPrompt(playerAvatar);
+    this.npc3.displayInteractPrompt(playerAvatar);
+  }
+
+  // custom code here to do stuff upon exiting room
+  unload() {
+    // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
+    this.npc2.resetInteraction();
+    this.npc3.resetInteraction();
+  }
+
+  // custom code here to do stuff upon entering room
+  load() {
+    // pass to PNGRoom to load image
+    super.load();
+    
+    // Add custom code here for unloading
+  }
+
+  keyPressed() {
+    if(key === ' ') {
+      if(this.npc1.isInteracting(playerAvatar)) {
+        this.npc1.continueInteraction();
+      }
+      else if(this.npc2.isInteracting(playerAvatar)) {
+        this.npc2.continueInteraction();
+      }
+      else if(this.npc3.isInteracting(playerAvatar)) {
+        this.npc3.continueInteraction();
+      }
+    }
   }
 }
 
 class Tech3Room extends PNGRoom {
   preload() {
     // define class varibles here, load images or anything else
+    this.npc1 = new NPC("Tech5", 255, 330, 'assets/npc-tech5.png');
+    this.npc1.addSingleInteraction("Hey, I'm Philip.");
+    this.npc1.addSingleInteraction("Can I ask you a question... as a woman?");
+    this.npc1.addSingleInteraction("Is the pay gap really real?");
+    this.npc1.addSingleInteraction("Because Asian-Americans make more");
+    this.npc1.addSingleInteraction("than white people on average.");
+    this.npc1.addSingleInteraction("We work so hard...");
+    this.npc1.addSingleInteraction("All minorities should be like us.");
+
+    this.npc2 = new NPC("Tech6", width - 500, height/2, 'assets/npc-tech6.png');
+    this.npc2.addSingleInteraction("I\'m really lucky,");
+    this.npc2.addSingleInteraction("I was able to come here and go to college");
+    this.npc2.addSingleInteraction("that\'s how I got this job.");
+    this.npc2.addSingleInteraction("I send a lot of my paycheck back");
+    this.npc2.addSingleInteraction("home to support my family.");
+    
+    // setup flag, seto false
+    this.hasSetup = false;
   }
 
   // call the PNGRoom superclass's draw function to draw the background image
   // and draw our code adter this
   draw() {
+    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    if( this.hasSetup === false ) {
+      // setup NPC 1
+      this.npc1.setup();
+      this.npc1.setPromptLocation(0,-170);
+      
+      // setup NPC 2
+      this.npc2.setup();
+      this.npc2.setPromptLocation(0,-170);
+
+      this.hasSetup = true; 
+    }
+
     // this calls PNGRoom.draw()
     super.draw();
 
-    // Add your code here
-    image(tech5, width/3, height/2);
-    image(tech6, width - 500, height/2);
+    // draw our NPCs
+    drawSprite(this.npc1.sprite);
+    drawSprite(this.npc2.sprite);
+
+    // When you have multiple NPCs, you can add them to an array and have a function 
+    // iterate through it to call this function for each more concisely.
+    this.npc1.displayInteractPrompt(playerAvatar);
+    this.npc2.displayInteractPrompt(playerAvatar);
+  }
+
+  // custom code here to do stuff upon exiting room
+  unload() {
+    // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
+    this.npc2.resetInteraction();
+  }
+
+  // custom code here to do stuff upon entering room
+  load() {
+    // pass to PNGRoom to load image
+    super.load();
+    
+    // Add custom code here for unloading
+  }
+
+  keyPressed() {
+    if(key === ' ') {
+      if(this.npc1.isInteracting(playerAvatar)) {
+        this.npc1.continueInteraction();
+      }
+      else if(this.npc2.isInteracting(playerAvatar)) {
+        this.npc2.continueInteraction();
+      }
+    }
   }
 }
 
 class InHomeRoom extends PNGRoom {
   preload() {
     // define class varibles here, load images or anything else
+    this.npc1 = new NPC("Mom", 200, 500, 'assets/npc-mom.png');
+    this.npc1.addSingleInteraction("I could\'t get groceries today");
+    this.npc1.addSingleInteraction("There was a protest on the road.");
+    this.npc1.addSingleInteraction("It was scary, so many people.");
+    this.npc1.addSingleInteraction("They were yelling so loud...");
+    this.npc1.addSingleInteraction("making my life harder.");
+    this.npc1.addSingleInteraction("Why do they need to do that?");
+    this.npc1.addSingleInteraction("Why can\'t they just behave.");
+
+    this.npc2 = new NPC("Dad", width/2, height/3, 'assets/npc-dad.png');
+    this.npc2.addSingleInteraction("What\'s this on the TV?");
+    this.npc2.addSingleInteraction("When we first moved here,");
+    this.npc2.addSingleInteraction("we didn\'t have to do all this.");
+    this.npc2.addSingleInteraction("What do you mean that\'s not right?");
+    
+    // setup flag, seto false
+    this.hasSetup = false;
   }
 
   // call the PNGRoom superclass's draw function to draw the background image
   // and draw our code adter this
   draw() {
+    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    if( this.hasSetup === false ) {
+      // setup NPC 1
+      this.npc1.setup();
+      this.npc1.setPromptLocation(0,-170);
+      
+      // setup NPC 2
+      this.npc2.setup();
+      this.npc2.setPromptLocation(0,-170);
+
+      this.hasSetup = true; 
+    }
+
     // this calls PNGRoom.draw()
     super.draw();
 
-    // Add your code here
-    image(mom, width/3, height/3);
-    image(dad, width/2, height/3);
+    // draw our NPCs
+    drawSprite(this.npc1.sprite);
+    drawSprite(this.npc2.sprite);
+
+    // When you have multiple NPCs, you can add them to an array and have a function 
+    // iterate through it to call this function for each more concisely.
+    this.npc1.displayInteractPrompt(playerAvatar);
+    this.npc2.displayInteractPrompt(playerAvatar);
+
+    // start game
+    if(this.npc2.interactionIndex === 3){
+        drawHomeGrabbables();
+    }
+  }
+
+  drawHomeGrabbables(){
+
+  }
+
+  // custom code here to do stuff upon exiting room
+  unload() {
+    // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
+    this.npc2.resetInteraction();
+  }
+
+  // custom code here to do stuff upon entering room
+  load() {
+    // pass to PNGRoom to load image
+    super.load();
+    
+    // Add custom code here for unloading
+  }
+
+  keyPressed() {
+    if(key === ' ') {
+      if(this.npc1.isInteracting(playerAvatar)) {
+        this.npc1.continueInteraction();
+      }
+      if(this.npc2.isInteracting(playerAvatar)) {
+        this.npc2.continueInteraction();
+      }
+    }
   }
 }
 
 class ProtestRoom extends PNGRoom {
   preload() {
     // define class varibles here, load images or anything else
+    this.npc1 = new NPC("Activist", width-200, 330, 'assets/npc-activist.png');
+    this.npc1.addSingleInteraction("Hi! Did you just get here?");
+    this.npc1.addSingleInteraction("My name is Jackie, nice to meet you.");
+    this.npc1.addSingleInteraction("Thanks for bringing all these signs!");
+    this.npc1.addSingleInteraction("It is so amazing to see everyone here,");
+    this.npc1.addSingleInteraction("I\'m so tired of hearing how we have");
+    this.npc1.addSingleInteraction("to be quiet and listen to every rule.");
+    this.npc1.addSingleInteraction("Today\'s protest is to call for more");
+    this.npc1.addSingleInteraction("detailed race questions on the census.");
+    this.npc1.addSingleInteraction("This will help us break the model-");
+    this.npc1.addSingleInteraction("minority myth, so that\'s why you\'ll");
+    this.npc1.addSingleInteraction("see other signs here.");
+    this.npc1.addSingleInteraction("Hope to see you around!");
+
+    this.npc2 = new NPC("Protest", width/3, height-250, 'assets/npc-protest.png');
+    this.npc2.addSingleInteraction("STOP ASIAN HATE");
+    this.npc2.addSingleInteraction("WE WANT A MORE DETAILED CENSUS");
+    this.npc2.addSingleInteraction("NOT YOUR MODEL MINORITY");
+    this.npc2.addSingleInteraction("YELLOW PERIL FOR BLACK LIVES");
+    this.npc2.addSingleInteraction("WE ARE NOT ALL THE SAME");
+    
+    // setup flag, seto false
+    this.hasSetup = false;
   }
 
   // call the PNGRoom superclass's draw function to draw the background image
   // and draw our code adter this
   draw() {
+    // Idea is to call the npc1.setup() function ONE time, so we use this kind of flag
+    if( this.hasSetup === false ) {
+      // setup NPC 1
+      this.npc1.setup();
+      this.npc1.setPromptLocation(0,-225);
+      
+      // setup NPC 2
+      this.npc2.setup();
+      this.npc2.setPromptLocation(0,-250);
+
+      this.hasSetup = true; 
+    }
+
     // this calls PNGRoom.draw()
     super.draw();
 
-    // Add your code here
-    imageMode(CENTER);
-    image(protest, width/2, height - 330);
-    image(activist, width - 200, height/3);
+    // draw our NPCs
+    drawSprite(this.npc1.sprite);
+    drawSprite(this.npc2.sprite);
+
+    // When you have multiple NPCs, you can add them to an array and have a function 
+    // iterate through it to call this function for each more concisely.
+    this.npc1.displayInteractPrompt(playerAvatar);
+    this.npc2.displayInteractPrompt(playerAvatar);
   }
+
+  // custom code here to do stuff upon exiting room
+  unload() {
+    // reset NPC interaction to beginning when entering room
+    this.npc1.resetInteraction();
+    this.npc2.resetInteraction();
+  }
+
+  // custom code here to do stuff upon entering room
+  load() {
+    // pass to PNGRoom to load image
+    super.load();
+    
+    // Add custom code here for unloading
+  }
+
+  keyPressed() {
+    if(key === ' ') {
+      if(this.npc1.isInteracting(playerAvatar)) {
+        this.npc1.continueInteraction();
+      }
+      else if(this.npc2.isInteracting(playerAvatar)) {
+        this.npc2.continueInteraction();
+      }
+    }
+  }
+  //   image(protest, width/2, height - 330);
+  //   image(activist, width - 200, height/3);
+  // }
 }
 
 //-- MODIFY THIS: for your own classes
